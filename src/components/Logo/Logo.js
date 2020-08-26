9c4452ec0ed7f8843e6e8222a71b670133c3f033
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-26 17:50:17 +0300

    fix path

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-import soupLogo from '../../assets/images/';
+// import soupLogo from '../../assets/images/';
 import classes from './Logo.module.css';
+
+const soupLogo =
+  'https://cdn.dribbble.com/users/654369/screenshots/1855571/untitled-4.png';
 const logo = () => (
   <div className={classes.Logo}>
     <img src={soupLogo} alt="Soup logo" />
