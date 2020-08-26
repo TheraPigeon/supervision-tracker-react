a10bbd0fe3cdb152ae78558b2ccac99a2cd1ea4e
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-26 22:02:18 +0300

    configure redux/routes

--- src/hoc/App.js
+++ src/hoc/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './Layout/Layout';
-
+import Auth from '../containers/Auth/Auth';
+import Dashboard from '../containers/Dashboard/Dashboard';
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout>{/* Switch with Route */}</Layout>
+        <Layout>
+          <Switch>
+            <Route path="/auth" component={Auth} />
+            {/* <Route path="/logout" component={Logout} /> */}
+            <Route path="/" exact component={Dashboard} />
+          </Switch>
+        </Layout>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './hoc/App';
 import * as serviceWorker from './serviceWorker';
+import authReducer from './store/reducers/auth';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
