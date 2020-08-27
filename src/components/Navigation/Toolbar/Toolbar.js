fa534a11474bd82fca0514bc11a61984a6ec78ab
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-27 15:34:41 +0300

    add conditional toolbar style

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,10 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.module.css';
 
-const toolbar = () => (
+const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <Logo />
-    <span>Welcome, User!</span>
+    {props.isAuth ? <span>Welcome, User!</span> : null}
   </header>
 );
 
