0f48fec65e62ca329b040818eb966bf7428598e3
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-27 14:30:37 +0300

    add PrivateRoute component

---
+++ src/containers/Login/Auth/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isLoggedIn ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    isAuthorized: state.auth.token !== null,
+  };
+};
+export default connect(mapStateToProps)(PrivateRoute);
