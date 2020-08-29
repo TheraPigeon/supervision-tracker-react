94e0123dab5afdef88e093fe0f1de3f55501f2c3
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-29 14:34:28 +0300

    add Logout component

--- src/hoc/App.js
+++ src/hoc/App.js
@@ -6,6 +6,7 @@ import PrivateRoute from '../containers/Login/Auth/PrivateRoute/PrivateRoute';
 
 import Layout from './Layout/Layout';
 import Login from '../containers/Login/Login';
+import Logout from '../containers/Login/Logout/Logout';
 import Dashboard from '../containers/Dashboard/Dashboard';
 class App extends Component {
   render() {
@@ -14,6 +15,8 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
+
             {/* <Route path="/logout" component={Logout} /> */}
             <PrivateRoute
               auth={this.props.isAuthorized}
