0272bde774028f665aa0272245f89813cb32a8e7
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-29 15:48:49 +0300

    add NavigationItems component

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = (props) => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/roster">My Roster</NavigationItem>
+    <NavigationItem link="/members">All members</NavigationItem>
+    <NavigationItem link="/logout">Logout</NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
