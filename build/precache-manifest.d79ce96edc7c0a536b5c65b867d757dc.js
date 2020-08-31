fcdd9f08da672606225b42924d9800c63e153614
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-08-31 23:44:32 +0300

    Initial dist subtree commit

---
+++ build/precache-manifest.d79ce96edc7c0a536b5c65b867d757dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd83c62ead6ea6e5a54fb1a33c61221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf0cd81bf02ea061fd37",
+    "url": "/static/css/main.9172f3a9.chunk.css"
+  },
+  {
+    "revision": "8914f31ff970647070e5",
+    "url": "/static/js/2.f235649a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f235649a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0cd81bf02ea061fd37",
+    "url": "/static/js/main.c45a9b21.chunk.js"
+  },
+  {
+    "revision": "cf4a570b6e13cb9ec7d7",
+    "url": "/static/js/runtime-main.bb548303.js"
+  }
+]);
