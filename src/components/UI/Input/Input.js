b48047b125791a3159417c1a239bc324d8980a69
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-09-11 23:30:18 +0300

    add input radio type

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -13,6 +13,7 @@ const input = (props) => {
   ) {
     inputClasses.push(classes.Invalid);
   }
+
   switch (props.elementType) {
     case 'input':
       inputElement = (
@@ -60,12 +61,39 @@ const input = (props) => {
         />
       );
   }
-  return (
-    <div className={classes.Input}>
-      {inputElement}
-      <label className={classes.Label}>{props.label}</label>
-    </div>
-  );
+  let render = null;
+  if (props.elementConfig.type === 'radio') {
+    inputClasses.push(classes.Radio);
+
+    render = (
+      <fieldset className={classes.Fieldset}>
+        <h3>{props.elementConfig.question}</h3>
+        {props.elementConfig.options.map(([label, value]) => {
+          return (
+            <label className={classes.Label}>
+              <input
+                key={value}
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={value}
+                onChange={props.changed}
+                checked={props.value === value}
+              />
+              <span>{label}</span>
+            </label>
+          );
+        })}
+      </fieldset>
+    );
+  } else {
+    render = (
+      <div className={classes.Input}>
+        {inputElement}
+        <label className={classes.Label}>{props.label}</label>
+      </div>
+    );
+  }
+  return render;
 };
 
 export default input;
