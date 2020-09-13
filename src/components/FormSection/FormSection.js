89ae0dbc940af067fae748bdb0d97e91cad324fb
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-09-13 20:58:50 +0300

    add FormSection style

--- src/components/FormSection/FormSection.js
+++ src/components/FormSection/FormSection.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../components/UI/Input/Input';
 import classes from './FormSection.module.css';
 
-const formSection = (props) => {
+const FormSection = (props) => {
+  const [expanded, setExpanded] = useState(false);
   const formElementArray = [];
-  for (let key in props.questions) {
+  // for (let i = props.start - 1; i < props.end; i++) {
+  //   console.log(props.questions);
+  //   formElementArray.push({
+  //     id: key,
+  //     config: props.questions[key],
+  //   });
+  // }
+  const contentClasses = expanded
+    ? [classes.Content, classes.Expanded]
+    : [classes.Content];
+  const styleContent = expanded ? { height: '100%' } : null;
+  for (let key in props.questions[props.category]) {
     formElementArray.push({
       id: key,
-      config: props.questions[key],
+      category: props.category,
+      config: props.questions[props.category][key],
     });
   }
 
@@ -17,19 +30,25 @@ const formSection = (props) => {
       elementType={formElement.config.elementType}
       elementConfig={formElement.config.elementConfig}
       value={formElement.config.value}
-      changed={(e) => props.radioChangeHandler(e, formElement.id)}
+      changed={(e) =>
+        props.radioChangeHandler(e, formElement.category, formElement.id)
+      }
       shouldValidate={formElement.config.validation}
       invalid={!formElement.config.valid}
       touched={formElement.config.touched}
       label={formElement.config.label}
     />
   ));
+
   return (
     <div className={classes.FormSection}>
-      <h2>{props.children}</h2>
-      {form}
+      <h2 onClick={() => setExpanded(!expanded)}>
+        <span className={expanded ? classes.Rotate : null}></span>
+        {props.children}
+      </h2>
+      <div className={contentClasses.join(' ')}>{form}</div>
     </div>
   );
 };
 
-export default formSection;
+export default FormSection;
