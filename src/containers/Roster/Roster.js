0fa5f2eff070624a396f06df29ed7d683ac52880
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-09-27 00:46:26 +0300

    add fetch users for managing roster

--- src/containers/Roster/Roster.js
+++ src/containers/Roster/Roster.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Member from './Member/Member';
-import AddMember from './AddMember/AddMember';
 import classes from './Roster.module.css';
 import * as actions from '../../store/actions/index';
 
+import Modal from '../../components/UI/Modal/Modal';
+import Button from '../../components/UI/Button/Button';
+
 class Roster extends Component {
+  state = {
+    managingStaff: false,
+  };
+  componentDidMount = () => {
+    console.log('[Roster.js] componentDidMount');
+  };
+  handleModal = () => {
+    if (!this.state.managingStaff) {
+      this.props.fetchMembers(this.props.clinicId, this.props.token);
+    }
+    this.setState((prevState) => {
+      return { managingStaff: !prevState.managingStaff };
+    });
+    console.log('handleModal');
+  };
   render() {
     let roster = null;
     if (this.props.roster) {
@@ -14,16 +31,24 @@ class Roster extends Component {
         return <Member key={member} memberId={member} name={name} />;
       });
     }
+    let listOfUsers = this.props.members;
+    console.log(listOfUsers);
 
     return (
-      <div className={classes.Roster}>
-        <header>
-          <h1>My Roster</h1>
-          <AddMember />
-        </header>
-
-        {roster}
-      </div>
+      <React.Fragment>
+        <Modal show={this.state.managingStaff} modalClosed={this.handleModal}>
+          All RBT staff members here
+        </Modal>
+        <div className={classes.Roster}>
+          <header>
+            <h1>My Roster</h1>
+            <Button clicked={this.handleModal}>
+              <i className="fa fa-cog"></i>Manage
+            </Button>
+          </header>
+          {roster}
+        </div>
+      </React.Fragment>
     );
   }
 }
@@ -31,12 +56,17 @@ class Roster extends Component {
 const mapStateToProps = (state) => {
   return {
     roster: state.auth.roster,
+    members: state.allmembers.members,
+    clinicId: state.auth.currentClinic,
+    token: state.auth.token,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     setCurrectClinic: dispatch(actions.setCurrentClinic),
+    fetchMembers: (clinicId, token) =>
+      dispatch(actions.fetchMembers(clinicId, token)),
   };
 };
 
