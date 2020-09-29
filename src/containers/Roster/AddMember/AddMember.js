14d66509ecfd289a8f41adc811061dc54ca9f296
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-09-30 01:22:20 +0300

    add AddMember component

--- src/containers/Roster/AddMember/AddMember.js
+++ src/containers/Roster/AddMember/AddMember.js
@@ -2,32 +2,53 @@ import React, { useState } from 'react';
 import classes from './AddMember.module.css';
 // import Backdrop from '../../../components/UI/Backdrop/Backdrop';
 // import Input from '../../../components/UI/Input/Input';
+import Button from '../../../components/UI/Button/Button';
 
 const AddMember = (props) => {
   const [adding, setAdding] = useState(false);
+  const [name, setName] = useState('');
+  const [init, setInit] = useState('');
+  const [follow, setFollow] = useState(false);
   const addBtn = (
     <React.Fragment>
-      <i className="fa fa-cog"></i>
-      <p>Manage</p>
+      <Button clicked={() => setAdding(!adding)}>Add New Member</Button>
     </React.Fragment>
   );
   const submitHandler = (e) => {
     e.preventDefault();
+    console.log(name, init, follow);
     setAdding(false);
   };
   const form = (
     <React.Fragment>
-      {/* <Backdrop show={adding} clicked={() => setAdding(false)} /> */}
       <form onSubmit={(e) => submitHandler(e)}>
-        <input placeholder="Name" />
-        <input placeholder="Init" />
-        <button>add</button>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <input
+          value={init}
+          onChange={(e) => setInit(e.target.value)}
+          placeholder="Init"
+        />
+        <label>
+          Follow?
+          <input
+            value={follow}
+            onChange={(e) => setFollow(e.target.checked)}
+            type="checkbox"
+          />
+        </label>
+        <Button>Add</Button>
+        <Button clicked={() => setAdding(false)}>Cancel</Button>
       </form>
     </React.Fragment>
   );
   return (
-    <div className={classes.AddMember} onClick={() => setAdding(true)}>
-      {adding ? form : addBtn}
+    <div className={classes.AddMember}>
+      {!adding ? addBtn : null}
+      {adding ? form : null}
     </div>
   );
 };
