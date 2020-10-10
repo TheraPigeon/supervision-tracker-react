6e7cc45464acf75f3733058f722e36790a5f198a
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-10-10 15:50:39 +0300

    add fetchSoup reducer

--- src/store/reducers/history.js
+++ src/store/reducers/history.js
@@ -5,6 +5,7 @@ const initialState = {
   supervisions: null,
   loading: false,
   error: null,
+  soup: null,
 };
 
 const fetchSupervisionsStart = (state, action) => {
@@ -20,6 +21,19 @@ const fetchSupervisionsFail = (state, action) => {
   return updateObject(state, { error: action.error, loading: false });
 };
 
+const fetchSoupStart = (state, action) => {
+  return updateObject(state, { loading: true });
+};
+const fetchSoupSuccess = (state, action) => {
+  return updateObject(state, {
+    soup: action.soup,
+    loading: false,
+  });
+};
+const fetchSoupFail = (state, action) => {
+  return updateObject(state, { error: action.error, loading: false });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_SUPERVISIONS_START:
@@ -28,6 +42,12 @@ const reducer = (state = initialState, action) => {
       return fetchSupervisionsSuccess(state, action);
     case actionTypes.FETCH_SUPERVISIONS_FAIL:
       return fetchSupervisionsFail(state, action);
+    case actionTypes.FETCH_SOUP_START:
+      return fetchSoupStart(state, action);
+    case actionTypes.FETCH_SOUP_SUCCESS:
+      return fetchSoupSuccess(state, action);
+    case actionTypes.FETCH_SOUP_FAIL:
+      return fetchSoupFail(state, action);
     default:
       return state;
   }
