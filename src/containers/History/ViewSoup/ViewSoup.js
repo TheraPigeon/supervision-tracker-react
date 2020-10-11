36d0feb1085044a495af3b7bbe14163ebe480983
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-10-12 00:27:15 +0300

    add ViewSoup component

--- src/containers/History/ViewSoup/ViewSoup.js
+++ src/containers/History/ViewSoup/ViewSoup.js
@@ -1,22 +1,89 @@
-import { VERSION } from 'lodash';
 import React, { Component } from 'react';
-import axios from '../../../axios-soup'
+import { connect } from 'react-redux';
+import { format, getMilliseconds } from 'date-fns';
+
+import { formatToTimeZone } from 'date-fns-timezone';
+import * as actions from '../../../store/actions/index';
+
+import ViewSection from './ViewSection/ViewSection';
 
 class ViewSoup extends Component {
-    componentDidUpdate = () => {
-        if(this.props.soupId){
-            axios.get('api/supervisions/' + this.props.soupId)
-            .then(res => {
-                console.log(res)
-            })
-            .catch(err => {
-                console.log(err)
-        })
-      }
+  componentDidUpdate = (prevState) => {
+    if (this.props.soupId !== prevState.soupId) {
+      this.props.onFetchSoup(this.props.soupId);
+      console.log('[ViewSoup.js] - componentDidUpdate');
     }
-    render(){
-        return (<div>{this.props.soupId}</div>)
+  };
+
+  render() {
+    console.log(this.props.soup);
+    let soup = null;
+    if (this.props.soup) {
+      const {
+        date,
+        start_time,
+        end_time,
+        group,
+        telehealth,
+        supervisor_id,
+        staff_member_id,
+        json,
+      } = this.props.soup;
+      const startTime = formatToTimeZone(new Date(start_time), 'HH:mm', {
+        timeZone: 'Africa/Conakry',
+      });
+      const endTime = formatToTimeZone(new Date(end_time), 'HH:mm', {
+        timeZone: 'Africa/Conakry',
+      });
+      const soupDate = format(new Date(date), 'MMM d, yyyy');
+
+      const category = ['starting', 'main', 'ending', 'additional'];
+      const soupSections = category.map((soupSection, i) => {
+        return (
+          <ViewSection
+            key={soupSection + i}
+            category={soupSection}
+            questions={json}
+          />
+        );
+      });
+      soup = (
+        <React.Fragment>
+          <header>
+            <div>
+              <span>Date: {soupDate}</span>
+              <span>Session started at: {startTime}</span>
+              <span>Session ended at: {endTime}</span>
+            </div>
+            <div>
+              <span>Session was performed in {group ? 'group' : 'solo'}</span>
+              <span>
+                It was conducted {telehealth ? 'remotely' : 'in-person'}
+              </span>
+              <span>Therapist: {supervisor_id}</span>
+              <span>Clinician: {staff_member_id}</span>
+            </div>
+          </header>
+          <section>
+            <table>{soupSections}</table>
+          </section>
+        </React.Fragment>
+      );
     }
+    return <div>{soup}</div>;
+  }
 }
 
-export default ViewSoup;
+const mapStateToProps = (state) => {
+  return {
+    soup: state.history.soup,
+  };
+};
+
+const mapDispatchToPropst = (dispatch) => {
+  return {
+    onFetchSoup: (soupId) => dispatch(actions.fetchSoup(soupId)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToPropst)(ViewSoup);
