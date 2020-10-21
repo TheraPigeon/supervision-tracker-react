457320ed952550c83698490ea363b09713f37057
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-10-22 01:00:38 +0300

    add Reset component

---
+++ src/containers/Login/Reset/Reset.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './Reset.module.css';
+
+class Reset extends Component {
+  render() {
+    return (
+      <div className={classes.Reset}>
+        <h1>Reset Password</h1>
+      </div>
+    );
+  }
+}
+
+export default Reset;
