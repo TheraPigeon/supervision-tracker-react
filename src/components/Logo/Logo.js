a0cb4351c54673e8f8d0a3885978f32b5c8900a4
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-12-28 17:50:32 +0200

    change logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,15 +3,24 @@ import React from 'react';
 // import soupLogo from '../../assets/images/';
 import classes from './Logo.module.css';
 
-const soupLogo =
-  'https://cdn.dribbble.com/users/654369/screenshots/1855571/untitled-4.png';
-const logo = () => (
-  <div className={classes.Logo}>
-    {/* <img src={soupLogo} alt="Soup logo" /> */}
-    <h1>
-      SOUP <span>by TheraPigeon&#8482;</span>
-    </h1>
-  </div>
-);
+// const soupLogo =
+//   'https://cdn.dribbble.com/users/654369/screenshots/1855571/untitled-4.png';
+const logo = (props) => {
+  const style = props.authed
+    ? {
+        color: '#777',
+        webkitTextFillColor: 'unset',
+        background: 'transparent',
+      }
+    : null;
+  return (
+    <div className={classes.Logo}>
+      {/* <img src={soupLogo} alt="Soup logo" /> */}
+      <h1>
+        SOUP <span style={style}>by TheraPigeon&#8482;</span>
+      </h1>
+    </div>
+  );
+};
 
 export default logo;
