d53ca89fd856006978e6f0dc87d0ba544a294a4f
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2021-01-17 21:59:46 +0200

    add app test

--- src/hoc/App.test.js
+++ src/hoc/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import Layout from './Layout/Layout';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('renders with or without a name', () => {
+  act(() => {
+    render(
+      <Provider>
+        <MemoryRouter>
+          <Layout />
+        </MemoryRouter>
+      </Provider>,
+      container
+    );
+  });
+  expect(container.textContent).toBe('Welcome');
 });
